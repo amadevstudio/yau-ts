a6e047448dd6f91e71791eeae56fc02197c411e1
kinton <kintonk@yandex.ru>
2024-12-30 02:58:12 +0300

    Improve i18n: better types, numeric

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # yau
 
-Deep alpha version, can only send messages 
+Deep alpha version, can only send messages  
 
 Using asdf plugin manager  
 `asdf install nodejs`
 
 Simple example
+
 - main.ts
+
 ```ts
 import dotenv from 'dotenv';
 import path from 'path';
@@ -35,7 +37,9 @@ const botConfig: BotConfig<AvailableRoutes> = {
 
 initializeBot(ENV.BOT_TOKEN, botConfig);
 ```
+
 - i18n.ts
+
 ```ts
 import { Dictionary } from '@framework/i18n/setup';
 
@@ -46,14 +50,28 @@ export default function <AvailableLanguages extends string = string>({
 }): Dictionary<AvailableLanguages> {
   return {
     start: {
-      message: {
-        en: 'Welcome! This is the /start answer',
+      s: {
+        message: {
+          en: `Welcome to ${appName}! This is the /start answer. We have `,
+        },
       },
+      n: {
+        message: {
+          singular: {
+            en: '{0} star',
+          },
+          unite: {
+            en: '{0} stars'
+          }
+        }
+      }
     },
   };
 }
 ```
+
 - routes.ts
+
 ```ts
 import { Routes } from '@framework/core/types';
 import start from './user/start';
@@ -68,14 +86,16 @@ export const routes: Routes<AvailableRoutes> = {
   },
 };
 ```
+
 - start.ts
+
 ```ts
 import { ConstructedParams } from "@framework/core/types";
 
 export default async function start(d: ConstructedParams) {
   await d.outerSender(d.chat.id, [{
     type: 'text',
-    text: d.i18n.t(['start', 'message'])
+    text: d.i18n.t(['start', 's', 'message']) + d.i18n.t(['start', 'n', 'message'], { num: 2, vars: ["2"] })
   }]);
 }
 ```
--- src/i18n/setup.ts
+++ src/i18n/setup.ts
@@ -1,33 +1,171 @@
 import { z } from 'zod';
 import { TLogger } from '@framework/toolbox/logger';
 
-export type Dictionary<AvailableLanguages extends string = string> = {
-  [key in string]: Dictionary | { [key in AvailableLanguages]: string };
+class TranslationEmpty extends Error {
+  constructor(msg: string) {
+    super(msg);
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, TranslationEmpty.prototype);
+  }
+}
+
+type DefaultAvailableLanguages = 'en' | 'cn'; // Default available languages
+
+type LangDict<Languages extends string> = {
+  [key: string]: LangDict<Languages> | { [key in Languages]: string };
+};
+
+type Numerals = 'singular' | 'few' | 'many';
+type UniteNumerals = 'unite';
+type Numeral<Languages extends string> = Record<
+  Numerals,
+  { [k in Languages]: string }
+>;
+type NumeralOptional<Languages extends string> = Record<
+  Numerals,
+  { [k in Languages]?: string }
+>;
+type UniteNumeral<Languages extends string> = {
+  [k in UniteNumerals]: {
+    [k in Languages]?: string;
+  };
+};
+
+type NumeralAll<Languages extends string> =
+  | Numeral<Languages>
+  | (UniteNumeral<Languages> & { [k in Numerals]?: never })
+  | (UniteNumeral<Languages> & NumeralOptional<Languages>);
+type NumeralDict<Languages extends string> = {
+  [key: string]: NumeralDict<Languages> | NumeralAll<Languages>;
+};
+
+export type Dictionary<
+  ProjectAvailableLanguages extends string = DefaultAvailableLanguages
+> = {
+  [key: string]: {
+    s: LangDict<ProjectAvailableLanguages>;
+  } & {
+    n: NumeralDict<ProjectAvailableLanguages>;
+  };
 };
 
 const FinalTranslationMapSchema = z.record(z.string(), z.string());
 
-export default function (dictionary: Dictionary): InitializeI18n {
+function getNumericType(num: number): Numerals {
+  const db100 = num % 100;
+  if (11 <= db100 && db100 <= 14) return 'many';
+
+  const db10 = num % 10;
+  if (db10 == 1) return 'singular';
+
+  if (2 <= db10 && db10 <= 4) return 'few';
+
+  return 'many';
+}
+
+function uniteSafe<Languages extends string = DefaultAvailableLanguages>(
+  entity: NumeralAll<Languages>,
+  languageCode: Languages,
+  numeralType: Numerals
+): string | undefined {
+  if (!(languageCode in entity[numeralType]!)) {
+    return (entity as UniteNumeral<Languages>)?.unite?.[languageCode];
+  }
+
+  return entity[numeralType]?.[languageCode];
+}
+
+function insertVars(template: string, vars?: string[]) {
+  if (vars === undefined) {
+    return template;
+  }
+
+  let result = template;
+
+  for (let i = 0; i < vars.length; i++) {
+    result = result.replace(`{${i}}`, vars[i]);
+  }
+
+  return result;
+}
+
+export default function setupI18n<
+  Languages extends string = DefaultAvailableLanguages
+>(
+  dictionary: Dictionary<Languages>,
+  initialParams: { fallbackLanguageCode?: Languages } = {}
+): InitializeI18n {
   return {
     t:
       (frameworkLogger: TLogger, languageCode: string) =>
-      (id: string[]): string => {
+      (
+        id: string[],
+        params: { num?: number; vars?: string[] } = {}
+      ): string => {
         try {
-          let currMap: Dictionary | undefined = undefined;
+          let currMap: { [k: string]: string } | undefined = undefined;
           for (const k of id) {
             if (!FinalTranslationMapSchema.safeParse(dictionary[k]).success) {
-              currMap = (currMap ?? dictionary)[k] as Dictionary;
+              currMap = (currMap ?? dictionary)[k] as unknown as { [k: string]: string };
             }
           }
 
           if (currMap === undefined) {
-            frameworkLogger.error("Curr map is undefined", id.join('.'));
+            frameworkLogger.error('Curr map is undefined', id.join('.'));
             return id.join('.');
           }
 
-          return FinalTranslationMapSchema.parse(currMap)[languageCode];
+          const translationEmptyString = () =>
+            `Translation is undefined for id [${id.join(
+              ' '
+            )}], lang ${languageCode}, params ${JSON.stringify(params)}`;
+
+          const postProcess = (result: string) =>
+            insertVars(result, params.vars);
+
+          // Numeric translation
+          if (params.num !== undefined) {
+            const numericType = getNumericType(params.num);
+
+            const translation = uniteSafe(
+              currMap as unknown as UniteNumeral<Languages>,
+              languageCode as Languages,
+              numericType
+            );
+            if (translation !== undefined) return postProcess(translation);
+
+            if (initialParams.fallbackLanguageCode !== undefined) {
+              const fallbackTranslation = uniteSafe(
+                currMap as unknown as UniteNumeral<Languages>,
+                initialParams.fallbackLanguageCode as Languages,
+                numericType
+              );
+              if (fallbackTranslation !== undefined)
+                return postProcess(fallbackTranslation);
+            }
+
+            throw new TranslationEmpty(translationEmptyString());
+          }
+
+          // Non-numeric translation
+          return (() => {
+            const schema = FinalTranslationMapSchema.parse(currMap);
+            if (languageCode in schema) {
+              return postProcess(schema[languageCode]);
+            }
+
+            if (
+              initialParams.fallbackLanguageCode !== undefined &&
+              initialParams.fallbackLanguageCode in schema
+            ) {
+              return postProcess(schema[initialParams.fallbackLanguageCode]);
+            }
+
+            throw new TranslationEmpty(translationEmptyString());
+          })();
         } catch (error) {
-          frameworkLogger.error(error, id.join('.'));
+          frameworkLogger.error(error);
           return id.join('.');
         }
       },
@@ -52,9 +190,9 @@ export type InitializeI18n = {
   t: (
     frameworkLogger: TLogger,
     languageCode: string
-  ) => (id: string[]) => string;
+  ) => (id: string[], params?: { num?: number; vars?: string[] }) => string;
 };
 
 export type I18n = {
-  t: (id: string[]) => string;
+  t: (id: string[], params?: { num?: number; vars?: string[] }) => string;
 };
