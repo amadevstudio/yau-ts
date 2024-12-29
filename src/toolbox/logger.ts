d9accfeff99bce1a90093fe2c77a7fe5d4583cd1
kinton <kintonk@yandex.ru>
2024-12-29 23:50:31 +0300

    Refactor logger

--- src/toolbox/logger.ts
+++ src/toolbox/logger.ts
@@ -1,6 +1,6 @@
 import { Logger } from "tslog";
 
-export type TLogger = Logger<any>;
+export type TLogger = Logger<unknown>;
 
 export default function initializeLogger(): TLogger {
   return new Logger({ name: "frameworkLogger" });
